cc39c67b3743980eb57111227d75fc0d9c7f70b5
Bob <aboxnb@hotmail.ca>
2018-04-01 17:09:41 -0400

    Completed Sticky Header Scrolling

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,8 +1,12 @@
-
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
+import StickyHeader from './modules/StickyHeader';
 import $ from 'jquery';
 
+console.log('inside the App.js');
 var mobileMenu = new MobileMenu();
 new RevealOnScroll($(".feature-item"), "85%");
 new RevealOnScroll($(".testimonial"), "85%");
+console.log('inside the App.js - before Sticky');
+var stickyHeader = new StickyHeader();
+console.log('inside the App.js - after Sticky');
